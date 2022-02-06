69ffeba519cc44be4eee298a979f9539730bf941
Krzysztof Badower <kbadower@gmail.com>
2022-02-06 22:34:11 +0100

    more changes (#1)
    Co-authored-by: Krzysztof Badower <krzysztof.badower@gmail.com>

---
+++ Framework/Models/Card.cs
@@ -0,0 +1,11 @@
+﻿namespace Framework.Models
+{
+    public class Card
+    {
+        public virtual string Name { get; set; }
+        public virtual int Cost { get; set; }
+        public virtual string Rarity { get; set; }
+        public virtual string Arena { get; set; }
+        public virtual string Type{ get; set; }
+    }
+}
